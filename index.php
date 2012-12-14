5e4f8b0cca694394eb270d413152d46ef63791e9
Martin Joiner <martin@martinjoiner.co.uk>
2012-12-14 18:24:27 +0000

    Added year lines and offset

--- index.php
+++ index.php
@@ -13,12 +13,22 @@
 <body>
 	<h1>Timeline</h1>
 
-	<input type="number" id="scale" name="scale" value="10">
+	<label for="scale">Scale</label><input type="number" id="scale" name="scale" value="26">
+	<label for="offset">Offset</label><input type="number" id="offset" name="offset" value="82">
+
+	<div class="newyear" data-start="631152000">1990</div>
+	<div class="newyear" data-start="946684800">2000</div>
+	<div class="newyear" data-start="1104537600">2005</div>
+
+	<div class="newyear" data-start="1230768000">2009</div>
+	<div class="newyear" data-start="1262304000">2010</div>
+	<div class="newyear" data-start="1293840000">2011</div>
+	<div class="newyear" data-start="1325376000">2012</div>
 
 	<?php 
 
 	include("db_connect.inc.php"); 
-	$q = mysql_query("	SELECT `category`.name AS category_name, event.`name`, event.`start`, event.`end`
+	$q = mysql_query("	SELECT `category`.name AS category_name, event.`name`, event.`start`, event.`end`, event.colour
 						FROM event
 						LEFT JOIN `category` ON event.category_id = category.id
 						ORDER BY `category`.`id`
@@ -35,88 +45,18 @@
 			print '<div class="resizable ui-widget-content cat'.$counter++.'" data-height="90">';
 			print '<h2>'.$r["category_name"].'</h2>';
 		}
-		print '<div class="element" data-start="'.$r["start"].'" data-end="'.$r["end"].'"><h3>'.$r["name"].'</h3>
-			<div class="arrow"></div></div>';
+		print '<div class="element" data-start="'.$r["start"].'" data-end="'.$r["end"].'" style="background-color: #'.$r["colour"].';">
+				<div class="start date">'.date("M-d-Y",$r["start"]).'</div>
+				<div class="end date">'.date("M-d-Y",$r["end"]).'</div>
+				<h3>'.$r["name"].'</h3>
+			</div>';
 	}
 	print '</div>';
 	?>
- 
-	<div class="resizable ui-widget-content cat<?php print $counter++; ?>" data-height="90">
-		<div class="element" data-start="0" data-duration="200">
-			<div class="start date">12 Nov 2011</div>
-			<div class="end date">1 January 2012</div>
-			<h3>Job 1</h3>
-			<div class="arrow"></div>
-		</div>
-		<div class="element" data-start="210" data-duration="100">
-			<div class="start date">12 Nov 2011</div>
-			<div class="end date">1 January 2012</div>
-			<h3>Job 2</h3>
-			<div class="arrow"></div>
-		</div>
-		<div class="element" data-start="302" data-duration="200">
-			<div class="arrow"></div>
-		</div>
-		<div class="element" data-start="600" data-duration="200">
-			<div class="arrow"></div>
-		</div>
-	</div>
-
-	<div class="resizable ui-widget-content cat2" data-height="160">
-		<div class="element" data-start="10" data-duration="143">
-			<div class="start date">12 Nov 2011</div>
-			<div class="end date">1 January 2012</div>
-			<h3>Girl Number 1</h3>
-			<div class="arrow"></div>
-		</div>
-		<div class="element" data-start="160" data-duration="643">
-			<div class="start date">12 Nov 2011</div>
-			<div class="end date">1 January 2012</div>
-			<h3>Girl Number 2</h3>
-			<div class="arrow"></div>
-		</div>
-		<div class="element" data-start="900" data-duration="180"></div>
-		
-		<div class="element" data-start="900" data-duration="239"></div>
-	</div>
-
-	<div class="resizable ui-widget-content cat3" data-height="120">
-		<div class="element" data-start="0" data-duration="14">
-			<div class="start date">May 1983</div>
-			<div class="end date">July 1983</div>
-			<h2>Born in Salisbury</h2>
-		</div>
-		<div class="element" data-start="14" data-duration="1008">
-			<div class="start date">July 1983</div>
-			<div class="end date">September 2001</div>
-			<h2>Parents house, Taunton</h2>
-		</div>
-		<div class="element" data-start="1355492098" date-end="1355492174">
-			<div class="start date">September 2001</div>
-			<div class="end date">June 2002</div>
-			<h2>Student house, Plymouth</h2>
-		</div>
-		<div class="element" data-start="1062" data-duration="40">
-			<div class="start date">September 2002</div>
-			<div class="end date">June 2003</div>
-			<h2>2nd student house, Plymouth</h2>
-		</div>
-		<div class="element" data-start="1084" data-duration="3">
-			<div class="start date">June 2002</div>
-			<div class="end date">July 2003</div>
-			<h2>Shared house, London</h2>
-		</div>
-		<div class="element" data-start="900" data-duration="200"></div>
-		<div class="element" data-start="1200" data-duration="200"></div>
-	</div>
 
 	<div>
 		<input type="date">
 	</div>
-
-	<footer class="post-info">
-
-	</footer>
 		
 </body>
 </html>
--- js/timeline.js
+++ js/timeline.js
@@ -1,46 +1,58 @@
 
-    function duration(start,end,unit){
-    	var difSecs = end - start;
-    	var difHours = difSecs / 3600;
-    	var difDays = difHours / 24;
-    	var difWeeks = difDays / 7;
-  		switch(unit){
-            case 'secs': return difSecs; break;
-            case 'days': return difDays; break;
-  			case 'weeks': return difWeeks; break;
-  		}
-    }
+var earliestdate = "1355961600"; // Todays timestamp by default
 
-    $(function() {
+function duration(start,end,unit){
+	var difSecs = end - start;
+	var difHours = difSecs / 3600;
+	var difDays = difHours / 24;
+	var difWeeks = difDays / 7;
+		switch(unit){
+        case 'secs': return difSecs; break;
+        case 'days': return difDays; break;
+			case 'weeks': return difWeeks; break;
+		}
+}
 
-        $(".element").mouseover( function(){$(this).children('.date').show() }).mouseout(function(){$(this).children('.date').hide() });
-        
-    	rescale(10);
-		$(".resizable").each( function(){
-			$(this).css('height',$(this).data('height')).children('.element').css('height',$(this).data('height')-35);
-		} );
-        $( ".resizable" ).resizable({
-		    start:function () {
-		      
-		    },
-		    stop:function () {
-		     
-		    },
-		    resize:function () {
-			  $(this).children('.element').css('height',parseInt($(this).css('height').substring(0,$(this).css('height').length-2))-35);
-			  
-		    }
-		  });
+$(function() {
 
-    	$('#scale').change( function(){ rescale($(this).attr('value')) } );
+    $(".element").mouseover( function(){$(this).children('.date').show() }).mouseout(function(){$(this).children('.date').hide() });
+    $(".element").each( function(){ 
+        if($(this).data('start') < earliestdate){
+            earliestdate = $(this).data('start');
+        }
     });
+	rescale();
+	$(".resizable").each( function(){
+		$(this).css('height',$(this).data('height')).children('.element').css('height',$(this).data('height')-24);
+	});
+    $( ".resizable" ).resizable({
+	    start:function () {
+	      
+	    },
+	    stop:function () {
+	     
+	    },
+	    resize:function () {
+		  $(this).children('.element').css('height',parseInt($(this).css('height').substring(0,$(this).css('height').length-2))-24);
+		  
+	    }
+	  });
 
-    function rescale(scale){
-    	scale=scale/10;
-    	console.log(scale);
+	$('#scale').change( function(){ rescale() } );
+    $('#offset').change( function(){ rescale() } );
+});
 
-    	$(".element").each( function() {
-    		$(this).css('width',$(this).data('duration')*scale);
-    		$(this).css({left:$(this).data('start')*scale+'px'});
-    	});
-    }
+function rescale(){
+    var scale = $('#scale').attr('value')/10;
+	var offset = $('#offset').attr('value')*scale*10;
+	$(".element").each( function() {
+        $(this).data('duration',duration($(this).data('start'),$(this).data('end'),'weeks'));
+		$(this).css('width',$(this).data('duration')*scale);
+        $(this).data('left',duration(earliestdate,$(this).data('start'),'weeks'));
+		$(this).css({left:($(this).data('left')*scale)-offset+'px'});
+	});
+    $(".newyear").each( function(){
+        $(this).data('left',duration(earliestdate,$(this).data('start'),'weeks'));
+        $(this).css({left:($(this).data('left')*scale)-offset+'px'});
+    })
+}
