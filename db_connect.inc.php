06e5b2958f4c81d40e431c292a6b2595c248f0ad
Martin Joiner <martin@martinjoiner.co.uk>
2013-03-09 22:56:40 +0000

    Hidden database security from git pullers

--- db_connect.inc.php
+++ db_connect.inc.php
@@ -1,6 +1,7 @@
 <?php 
 
-$dbh = mysql_connect ("localhost", "timeline", "unsecure") or die ('I cannot connect to the database because: ' . mysql_error());
-mysql_select_db ("timeline"); 
+include("config.inc.php");
+$dbh = mysql_connect ("localhost", $db_username, $db_password) or die ('I cannot connect to the database because: ' . mysql_error());
+mysql_select_db ($db_database); 
 
 ?>
