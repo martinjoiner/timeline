e6131ad0253fe8ba155fe7d670a4b921e3267c9a
Martin Joiner <martin@martinjoiner.co.uk>
2015-01-16 15:52:13 +0000

    db Connect: Added warning if config.inc.php does not exist

--- public_html/db_connect.inc.php
+++ public_html/db_connect.inc.php
@@ -1,5 +1,10 @@
 <?php 
 
+if( !file_exists($_SERVER['DOCUMENT_ROOT'] . "/config.inc.php") ){
+	print "Error: You have not created config.inc.php in the site root. See config.inc.php.sample for instructions.";
+	exit;
+}
+
 include( $_SERVER['DOCUMENT_ROOT'] . "/config.inc.php");
 $dbh = mysql_connect ("localhost", $db_username, $db_password) or die ('I cannot connect to the database because: ' . mysql_error());
 mysql_select_db ($db_database); 
