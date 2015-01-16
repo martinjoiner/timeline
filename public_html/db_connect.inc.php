3f3dedeace8d639783f867ed19060a8f47f4e16d
Martin Joiner <martin@martinjoiner.co.uk>
2015-01-16 15:43:47 +0000

    db Connect: Config include used doc root

--- public_html/db_connect.inc.php
+++ public_html/db_connect.inc.php
@@ -1,7 +1,5 @@
 <?php 
 
-include("config.inc.php");
+include( $_SERVER['DOCUMENT_ROOT'] . "/config.inc.php");
 $dbh = mysql_connect ("localhost", $db_username, $db_password) or die ('I cannot connect to the database because: ' . mysql_error());
 mysql_select_db ($db_database); 
-
-?>
