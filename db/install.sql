7e370ce0757a93b541f1d7e891d8610dd4775721
Martin Joiner <martin@martinjoiner.co.uk>
2015-01-16 17:08:14 +0000

    Added a db install SQL file

---
+++ db/install.sql
@@ -0,0 +1,49 @@
+
+
+-- Table structure for table `category`
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `height` int(11) NOT NULL DEFAULT '90',
+  `priority` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `fk_category_user` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+
+-- Table structure for table `event`
+CREATE TABLE IF NOT EXISTS `event` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `start` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `end` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `colhex` varchar(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_event_category` (`category_id`),
+  KEY `fk_event_user` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+
+-- Table structure for table `user`
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(64) NOT NULL COMMENT 'The salted hash of the password',
+  `salt` varchar(32) NOT NULL COMMENT 'An MD5 hash of something',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+
+-- Constraints for table `category`
+ALTER TABLE `category`
+  ADD CONSTRAINT `fk_category_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+
+-- Constraints for table `event`
+ALTER TABLE `event`
+  ADD CONSTRAINT `fk_event_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_event_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
